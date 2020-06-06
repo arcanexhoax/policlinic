b089d835fcdce27e4d56e011d5ec6357551c29e0
arcanexhoax <62259600+arcanexhoax@users.noreply.github.com>
2020-06-07 02:16:21 +0300

    Moq was installed

---
+++ DAL.Tests/TestDoctorRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DAL.Repositories.Impl;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Tests {
+	class TestDoctorRepository : BaseRepository<Doctor>  {
+		public TestDoctorRepository(DbContext context)
+		: base(context) {
+		}
+	}
+}
